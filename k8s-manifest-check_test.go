7c2946a4d7e9f22cdac4076ead147fd3ed386292
Benjamin Borbe <bborbe@rocketnews.de>
2018-05-02 19:19:42 +0200

    check file exists

--- k8s-manifest-check_test.go
+++ k8s-manifest-check_test.go
@@ -88,7 +88,7 @@ spec:
 				Expect(err).To(BeNil())
 				serverSession.Wait(100 * time.Millisecond)
 				Expect(serverSession.ExitCode()).To(Equal(1))
-				Expect(serverSession.Buffer()).To(gbytes.Say("manifest not found"))
+				Expect(serverSession.Buffer()).To(gbytes.Say("manifest %s not found", manifestpath))
 			})
 		})
 	})
